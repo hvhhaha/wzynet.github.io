12d00c8975cf5706a98dc9f671ce9df8052784b8
hvhhaha <122376879+hvhhaha@users.noreply.github.com>
2025-01-01 13:37:58 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -10,7 +10,7 @@ const ap = new APlayer({
     audio: [{
             name: "遇见",
             artist: "孙燕姿",
-            url: "https://tools.liumingye.cn/music/api/link?id=m3f63OtUGNZoMDio1Kq9X7jE4kM2ddqh9eCynjQA7dWSL0qLZFeLha7Mu2hTNcZ8&quality=128&_t=1735702060711&token=20241016.ee5ab0004f5234351e6d3e2bc2e65816",
+            url: "https://lv-sycdn.kuwo.cn/e0e25e3408d8a9c71e73c30df9bab39e/6774bcd9/resource/30106/trackmedia/M500004RMLd70LGo7K.mp3?bitrate$128&from=vip",
             cover: "https://y.gtimg.cn/music/photo_new/T002R300x300M0000024bjiL2aocxT_1.jpg?max_age=2592000",
             lrc: "https://s-sh-2127-music.oss.dogecdn.com/lrc%2F%E5%91%A8%E6%9D%B0%E4%BC%A6-%E5%A4%9C%E6%9B%B2.lrc",
             theme: "#171513"
