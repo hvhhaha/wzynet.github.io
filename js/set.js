c94bd519adf9b1352e6d7c39202aa02a71e14176
hvhhaha <13688358926@163.com>
2025-01-02 11:57:42 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -51,7 +51,7 @@ function setBgImgInit() {
             $('#bg').attr('src', 'https://api.dujin.org/bing/1920.php'); //必应每日
             break;
         case "3":
-            $('#bg').attr('src', 'https://image.anosu.top/pixiv/direct?r18=1'); //随机涩t
+            $('#bg').attr('src', 'https://api.anosu.top/api?sort=r18'); //随机涩t
             break;
         case "4":
             $('#bg').attr('src', 'https://t.mwm.moe/pc'); //随机动漫
@@ -98,7 +98,7 @@ $(document).ready(function () {
 
         if (type === "3") {
             setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/gqapi/gqapi.php'); //随机风景
+            $('#bg').attr('src', 'https://api.anosu.top/api?sort=r18'); //随机s图
             iziToast.show({
                 message: '壁纸设置成功',
             });
@@ -106,7 +106,7 @@ $(document).ready(function () {
 
         if (type === "4") {
             setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/api/api.php'); //随机动漫
+            $('#bg').attr('src', 'https://t.mwm.moe/pc'); //随机动漫
             iziToast.show({
                 message: '壁纸设置成功',
             });
